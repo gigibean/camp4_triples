ee82b8eae667c04f9f3cfbe04ae629968dceb9a4
gigibean <rhalswl1@gmail.com>
2020-02-12 20:33:00 +0900

    update profile update (profilebase)

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('mr/', views.mr, name='mr'),
+    path('songs/', views.songs, name='songs'),
+    path('likes/', views.likes, name='likes'),
+    path('followings/', views.followings, name='followings'),
+    path('followers/', views.followers, name='followers'),
+    path('comments/', views.comments, name='comments'),
 ]
--- profilepage/views.py
+++ profilepage/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'profile.html', {'range': range(10), })
+
+
+def mr(request):
+    return render(request, 'mr.html')
+
+
+def songs(request):
+    return render(request, 'songs.html')
+
+
+def likes(request):
+    return render(request, 'likes.html')
+
+
+def followings(request):
+    return render(request, 'followings.html')
+
+
+def followers(request):
+    return render(request, 'followers.html')
+
+
+def comments(request):
+    return  render(request, 'comments.html')
