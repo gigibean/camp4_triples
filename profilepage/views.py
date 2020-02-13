4455609583dc8fff8460677c3701ffd01751d2ae
gigibean <rhalswl1@gmail.com>
2020-02-13 17:53:57 +0900

    profile complete v1

--- profilepage/views.py
+++ profilepage/views.py
@@ -3,28 +3,28 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return render(request, 'profile.html', {'range': range(10), })
+    return render(request, 'profile.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def mr(request):
-    return render(request, 'mr.html')
+    return render(request, 'mr.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def songs(request):
-    return render(request, 'songs.html')
+    return render(request, 'songs.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def likes(request):
-    return render(request, 'likes.html')
+    return render(request, 'likes.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def followings(request):
-    return render(request, 'followings.html')
+    return render(request, 'followings.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def followers(request):
-    return render(request, 'followers.html')
+    return render(request, 'followers.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
 
 
 def comments(request):
-    return  render(request, 'comments.html')
+    return  render(request, 'comments.html', {'range': range(10), 'sl_range': range(3), 'sfw_range': range(8), 'sfi_range': range(3)})
